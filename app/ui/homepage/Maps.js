4b05dbb9719779e37041d37237da0ceab8a3c6f7
markmumba <markmumba01@gmail.com>
2024-03-13 21:01:10 +0300

    making app responsive

--- app/ui/homepage/Maps.js
+++ app/ui/homepage/Maps.js
@@ -14,9 +14,9 @@ function ImageSection() {
 
   return (
     <div >
-      <div className="md:px-20 max-md:pt-10 px-5">
+      <div className="md:px-20 max-md:pt-10  px-5">
         <iframe
-        className="max-md:size-80 md:size-96 rounded-3xl"
+        className="sm:w-[600px] w-[350px] md:w-[700px] lg:w-[900px] xl:w-[1700px] rounded-3xl"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.4829772795483!2d36.89839333857789!3d-1.1843547497010776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xac76e4d9299c2443%3A0x655ebda43df91d3!2sLakewood%20Havens%20Kindergarden!5e0!3m2!1sen!2ske!4v1709318897816!5m2!1sen!2ske"
           height="450" 
           width="1700"
